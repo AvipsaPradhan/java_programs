76742e22b9b136346f9dc439dd77cc3329c85bd5
AvipsaPradhan <96482352+AvipsaPradhan@users.noreply.github.com>
2023-04-10 21:57:45 +0530

    Add files via upload

--- Distinctornot.java
+++ Distinctornot.java
@@ -2,12 +2,21 @@ package Arrays;
 
 public class Distinctornot {
 	public static boolean distinctornot(int arr[]) {
-		
+		int n=arr.length;
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(arr[i]==arr[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+        int arr[]= {11,2,3,1};
+        System.out.println(distinctornot(arr));
+        }
 
 }
